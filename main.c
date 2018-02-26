921d60f854327e92ad167beffee7428df53ecc17
Kevin Conti <marketing.KevinConti@gmail.com>
2018-02-26 17:50:29 -0500

    Added comments

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+//Author: Kevin Conti
+//Date Written: 02/26/2018
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,7 +9,10 @@ double globalDouble = 3.0;
 char globalChar = '3';
 void part1(int, double, char);
 int factorial(int);
+void part3();
 
+//Purpose: Calls all three of the methods in this file, each one performs one part of the assignment
+//For more detail see comments on each method
 int main() {
     int paramInt = 8, factorialValue;
     double paramDouble = 8.0;
@@ -14,9 +20,12 @@ int main() {
     part1(paramInt, paramDouble, paramChar);
     factorialValue = factorial(6);
     printf("\nFactorial value is: %d\n", factorialValue);
+    part3();
     return 0;
 }
 
+//Purpose: This method prints out the locations of global, static, local, parameter, and literal values
+//Besides being printed the parameters have no utility
 void part1(int paramInt, double paramDouble, char paramChar){
     int localInt;
     int *dynamicInt;
@@ -63,8 +72,10 @@ void part1(int paramInt, double paramDouble, char paramChar){
     printf("Literal: %p\n\n", literalCharPtr);
 }
 
-//This Algorithm returns n! factorial, where n = the integer sent in the parameter
+//Purpose: This Algorithm returns n! factorial, where n = the integer sent in the parameter
 //this algorithm is implemented in a recursive function
+//Parameter: power: the current value of n
+//Returns: The value of n!
 int factorial(int power){
     int factorialValue;
 
@@ -78,12 +89,30 @@ int factorial(int power){
 
         return factorialValue;
     } else {
-        //Progress step
+        //Progress step (n! = n * (n-1)!)
         factorialValue = factorial(power - 1) * power;
 
         //Print statements for part 2
         printf("factorialValue location: %p\n", (void *)&factorialValue);
         printf("factorialValue value: %d\n", factorialValue);
         return factorialValue;
+    }
+}
+
+//Purpose: Construct a multi-dimensional array, iterate through the array and print the location of each entry
+//The goal of this algorithm is to determine whether C uses row-major or column-major ordering for arrays
+void part3(){
+    double array[4][2][3];
+    int i, j, k, tables, rows, columns;
+    tables = sizeof(array) / sizeof(array[0]);
+    rows = sizeof(array[0]) / sizeof(array[0][0]);
+    columns = sizeof(array[0][0]) / sizeof(array[0][0][0]);
+    for(i = 0; i < tables; i++){
+        for(j=0; j < rows; j++){
+            for(k=0; k < columns; k++){
+                //If these print in order, C is row-major
+                printf("Address of [%d][%d][%d]: %p\n", i,j,k, (void *)&array[i][j][k]);
+            }
+        }
     }
 }
